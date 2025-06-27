c47fb154f8b3a4b75589798fa23b9e28fdcb9a97
bainco <Connor.bain@northwestern.edu>
2025-06-27 19:12:25 +0000

    Deploying to gh-pages from @ bain-commcs/bain-commcs.github.io@7812c01e6bdee376bc6fab9dcc17778d03e39225 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-communicating-computer-science",
+    title: "Communicating Computer Science",
     section: "Navigation",
     handler: () => {
       window.location.href = "/al-folio/";
